9630ac6ef5b61d5432f2b092a76c14a51c4f40f9
tarheelfan200 <lukechristopherbutler@gmail.com>
2025-11-27 11:24:41 -0500

    push 219 & 229

--- 219.py
+++ 219.py
@@ -1,5 +1,6 @@
 '''
-'hashmap'/'set'
+Result: Solved ~1 hour
+
 Given an integer array nums and an integer k, 
 return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
@@ -17,13 +18,22 @@ Output: false
 '''
 
 def containsNearbyDuplicate(nums: list[int], k: int) -> bool:
-    lastestSeen = {}
-    return True
+    latestSeen = {}
+    for number in range(len(nums)):
+
+        if nums[number] in latestSeen: # check if integer is already present
+            # check if new index - old index >= k
+            if  k >= number - latestSeen[nums[number]]:
+                return True
+
+        latestSeen[nums[number]] = number # add all last occurance of integer to value of key
+
+    return False
 
 
 print("TEST CASE 1:")
-nums = [1,2,3,1] 
-k = 3
+nums = [1,0,1,1]
+k = 1
 
 
 print(containsNearbyDuplicate(nums=nums, k=k))
